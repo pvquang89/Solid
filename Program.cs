4a808dcabe1ecc4370c7e47b018ab31c6f1a208a
pvquang89 <phamvanquang12042002@gmail.com>
2024-07-09 11:55:18 +0700

    exp code for Interface Segregation Principle

--- Program.cs
+++ Program.cs
@@ -1,7 +1,34 @@
 ﻿internal class Program
 {
+    // // Không áp dụng ISP
+    // public interface IUser
+    // {
+    //     void Login(string username, string password);
+    //     void Logout();
+    //     void ChangePassword(string newPassword);
+    //     void ViewProfile();
+    //     void AssignRole(string role);
+    //     void ResetPassword();
+    // }
+    // Áp dụng ISP
+    public interface IUser
+    {
+        void Login(string username, string password);
+        void Logout();
+        void ChangePassword(string newPassword);
+        void ViewProfile();
+    }
+
+    public interface IAdminUser : IUser
+    {
+        void AssignRole(string role);
+    }
+
+    public interface IPasswordResettableUser : IUser
+    {
+        void ResetPassword();
+    }
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
     }
 }
