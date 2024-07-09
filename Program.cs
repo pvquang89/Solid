6101fb89b12953c9d02652b9519618fdd962b2f5
pvquang89 <phamvanquang12042002@gmail.com>
2024-07-09 11:22:55 +0700

    exp code for Single Responsibility Principle

--- Program.cs
+++ Program.cs
@@ -1,5 +1,34 @@
 ﻿internal class Program
 {
+    //Single Responsibility Principle
+    //Mỗi class chỉ đảm nhiệm 1 trách nhiệm duy nhất, tức chỉ có lí do để thay đổi nó
+
+    //Trước khi áp dụng
+    //Class User có quá nhiểu phương thức không liên quan như SaveToDatabase, SendEmail
+    // public class User
+    // {
+    //     public string Name { get; set; }
+    //     public string Email { get; set; }
+    //     public void SaveToDatabase(){}
+    //     public void SendEmail(){}
+    // }
+
+    //Sau khi áp dụng
+    public class User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+
+    public class UserRepository
+    {
+        public void SaveToDatabase(User user) { }
+    }
+
+    public class EmailService
+    {
+        public void SendEmail(User user) { }
+    }
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
